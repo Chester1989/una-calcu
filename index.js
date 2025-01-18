16573e5c2c5bd6147ca29e00878d2e46dea7ca6d
alejandro gonzalez flores <agf_001@outlook.com>
2025-01-17 18:23:12 -0800

    primer commit de calcu

---
+++ index.js
@@ -0,0 +1,35 @@
+//este modulo (index.js)tiene como proposito recivir el imput
+//por el usuario
+//entenderlo y delegar la operacion matematica a quien corresponda
+
+const operaciones = require("./operaciones")
+function parsearTerminos(texto){
+   //mock
+    return{
+        primerTermino:45,
+segundoTermino:89,
+operacion:"+",
+    };
+}
+
+function ejecutarOperacion(objetoOperacion){
+    const mapa={
+        "+":operaciones.sumar,
+        "-":operaciones.restar,
+        "/":operaciones.dividir,
+        "*":operaciones.multiplicar,
+    }
+    const simbolo=objetoOperacion.operacion;
+    const ejecutor=mapa[simbolo];
+
+    return ejecutor(objetoOperacion.primerTermino,
+        objetoOperacion.segundoTermino);
+};
+
+function main(){
+const operacionParseada = parsearTerminos(process.argv[2]);
+const resultado=ejecutarOperacion(operacionParseada);
+console.log(resultado);
+}
+
+main();
